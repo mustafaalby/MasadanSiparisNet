9a092a6e324a81ec59aac249c1148c030f467039
mustafaalby <albymusty9561@gmail.com>
2020-07-10 21:14:53 +0300

    Some Css added, Restaurant menu page changed

---
+++ ProjectRestaurant/ProjectRestaurant.Data/Migrations/20200710092723_Init3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectRestaurant.Data.Migrations
+{
+    public partial class Init3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "ProductType",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "ProductType");
+        }
+    }
+}
---
+++ ProjectRestaurant/ProjectRestaurant.Data/Migrations/20200710124842_Init4.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectRestaurant.Data.Migrations
+{
+    public partial class Init4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "ProductType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "ProductType",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
